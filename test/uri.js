2a169fb82a2c0688ba4f3298d66deda86f1e822b
OstlerDev <OstlerDev@users.noreply.github.com>
2018-04-09 11:56:28 -0700

    Fix URI Tests

--- test/uri.js
+++ test/uri.js
@@ -16,24 +16,24 @@ describe('URI', function() {
 
     URI.parse.bind(URI, 'badURI').should.throw(TypeError);
 
-    uri = URI.parse('florincoin:');
+    uri = URI.parse('flo:');
     expect(uri.address).to.be.undefined();
     expect(uri.amount).to.be.undefined();
     expect(uri.otherParam).to.be.undefined();
 
-    uri = URI.parse('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
-    uri.address.should.equal('1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+    uri = URI.parse('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3');
+    uri.address.should.equal('oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3');
     expect(uri.amount).to.be.undefined();
     expect(uri.otherParam).to.be.undefined();
 
-    uri = URI.parse('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=123.22');
-    uri.address.should.equal('1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+    uri = URI.parse('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=123.22');
+    uri.address.should.equal('oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3');
     uri.amount.should.equal('123.22');
     expect(uri.otherParam).to.be.undefined();
 
-    uri = URI.parse('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=123.22' +
+    uri = URI.parse('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=123.22' +
                     '&other-param=something&req-extra=param');
-    uri.address.should.equal('1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+    uri.address.should.equal('oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3');
     uri.amount.should.equal('123.22');
     uri['other-param'].should.equal('something');
     uri['req-extra'].should.equal('param');
@@ -41,24 +41,24 @@ describe('URI', function() {
 
   // TODO: Split this and explain tests
   it('URIs can be validated statically (test vector)', function() {
-    URI.isValid('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj').should.equal(true);
-    URI.isValid('florincoin:mkYY5NRvikVBY1EPtaq9fAFgquesdjqECw').should.equal(true);
+    URI.isValid('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3').should.equal(true);
+    URI.isValid('flo:oVsYipkiU1z8K8vgWyM5ExoPE38DXtasEt').should.equal(true);
 
-    URI.isValid('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=1.2')
+    URI.isValid('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=1.2')
                 .should.equal(true);
-    URI.isValid('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=1.2&other=param')
+    URI.isValid('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=1.2&other=param')
                 .should.equal(true);
-    URI.isValid('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=1.2&req-other=param',
+    URI.isValid('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=1.2&req-other=param',
                 ['req-other']).should.equal(true);
-    URI.isValid('florincoin:mmrqEBJxUCf42vdb3oozZtyz5mKr3Vb2Em?amount=0.1&' +
+    URI.isValid('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=0.1&' +
                 'r=https%3A%2F%2Ftest.bitpay.com%2Fi%2F6DKgf8cnJC388irbXk5hHu').should.equal(true);
 
-    URI.isValid('florincoin:').should.equal(false);
-    URI.isValid('florincoin:badUri').should.equal(false);
-    URI.isValid('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfk?amount=bad').should.equal(false);
-    URI.isValid('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfk?amount=1.2&req-other=param')
+    URI.isValid('flo:').should.equal(false);
+    URI.isValid('flo:badUri').should.equal(false);
+    URI.isValid('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=bad').should.equal(false);
+    URI.isValid('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=1.2&req-other=param')
                 .should.equal(false);
-    URI.isValid('florincoin:?r=https%3A%2F%2Ftest.bitpay.com%2Fi%2F6DKgf8cnJC388irbXk5hHu')
+    URI.isValid('flo:?r=https%3A%2F%2Ftest.bitpay.com%2Fi%2F6DKgf8cnJC388irbXk5hHu')
                 .should.equal(false);
   });
 
@@ -69,7 +69,7 @@ describe('URI', function() {
   });
 
   it('do not need new keyword', function() {
-    var uri = URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+    var uri = URI('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3');
     uri.should.be.instanceof(URI);
   });
 
@@ -78,26 +78,26 @@ describe('URI', function() {
     var uri;
 
     it('parses address', function() {
-      uri = new URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+      uri = new URI('flo:FEw2LpKbdo3g4QHNDXj3rcDZ6bdcE6Nx8b');
       uri.address.should.be.instanceof(flocore.Address);
       uri.network.should.equal(Networks.livenet);
     });
 
     it('parses amount', function() {
-      uri = URI.fromString('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=123.22');
-      uri.address.toString().should.equal('1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+      uri = URI.fromString('flo:FEw2LpKbdo3g4QHNDXj3rcDZ6bdcE6Nx8b?amount=123.22');
+      uri.address.toString().should.equal('FEw2LpKbdo3g4QHNDXj3rcDZ6bdcE6Nx8b');
       uri.amount.should.equal(12322000000);
       expect(uri.otherParam).to.be.undefined();
     });
 
     it('parses a testnet address', function() {
-      uri = new URI('florincoin:mkYY5NRvikVBY1EPtaq9fAFgquesdjqECw');
+      uri = new URI('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3');
       uri.address.should.be.instanceof(flocore.Address);
       uri.network.should.equal(Networks.testnet);
     });
 
     it('stores unknown parameters as "extras"', function() {
-      uri = new URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=1.2&other=param');
+      uri = new URI('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=1.2&other=param');
       uri.address.should.be.instanceof(flocore.Address);
       expect(uri.other).to.be.undefined();
       uri.extras.other.should.equal('param');
@@ -105,12 +105,12 @@ describe('URI', function() {
 
     it('throws error when a required feature is not supported', function() {
       (function() {
-        return new URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=1.2&other=param&req-required=param');
+        return new URI('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=1.2&other=param&req-required=param');
       }).should.throw(Error);
     });
 
     it('has no false negative when checking supported features', function() {
-      uri = new URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=1.2&other=param&' +
+      uri = new URI('flo:oRUQ2eVAZu6CVRnM3V9BS7DeRPiRX2TTU3?amount=1.2&other=param&' +
                     'req-required=param', ['req-required']);
       uri.address.should.be.instanceof(flocore.Address);
       uri.amount.should.equal(120000000);
@@ -125,19 +125,19 @@ describe('URI', function() {
     var uri;
 
     uri = new URI({
-      address: '1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj'
+      address: 'FEw2LpKbdo3g4QHNDXj3rcDZ6bdcE6Nx8b'
     });
     uri.address.should.be.instanceof(flocore.Address);
     uri.network.should.equal(Networks.livenet);
 
     uri = new URI({
-      address: 'mkYY5NRvikVBY1EPtaq9fAFgquesdjqECw'
+      address: 'oWteCT5W2ec4KXYnGm9oxS2S7QozhrZcLE'
     });
     uri.address.should.be.instanceof(flocore.Address);
     uri.network.should.equal(Networks.testnet);
 
     uri = new URI({
-      address: '1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj',
+      address: 'oH1K8pEwHz6q6eFkz3JjRLqeXAggCNCM8o',
       amount: 120000000,
       other: 'param'
     });
@@ -148,13 +148,13 @@ describe('URI', function() {
 
     (function() {
       return new URI({
-        address: '1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj',
+        address: 'oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K',
         'req-required': 'param'
       });
     }).should.throw(Error);
 
     uri = new URI({
-      address: '1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj',
+      address: 'oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K',
       amount: 120000000,
       other: 'param',
       'req-required': 'param'
@@ -166,19 +166,19 @@ describe('URI', function() {
   });
 
   it('should support double slash scheme', function() {
-    var uri = new URI('florincoin://1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
-    uri.address.toString().should.equal('1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+    var uri = new URI('flo://oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K');
+    uri.address.toString().should.equal('oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K');
   });
 
   it('should input/output String', function() {
-    var str = 'florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?' +
+    var str = 'flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K?' +
               'message=Donation%20for%20project%20xyz&label=myLabel&other=xD';
     URI.fromString(str).toString().should.equal(str);
   });
 
   it('should input/output JSON', function() {
     var json = JSON.stringify({
-      address: '1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj',
+      address: 'oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K',
       message: 'Donation for project xyz',
       label: 'myLabel',
       other: 'xD'
@@ -187,12 +187,12 @@ describe('URI', function() {
   });
 
   it('should support numeric amounts', function() {
-    var uri = new URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=12.10001');
+    var uri = new URI('flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K?amount=12.10001');
     expect(uri.amount).to.be.equal(1210001000);
   });
 
   it('should support extra arguments', function() {
-    var uri = new URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?' +
+    var uri = new URI('flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K?' +
                       'message=Donation%20for%20project%20xyz&label=myLabel&other=xD');
 
     should.exist(uri.message);
@@ -207,38 +207,38 @@ describe('URI', function() {
 
   it('should generate a valid URI', function() {
     new URI({
-      address: '1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj',
+      address: 'oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K',
     }).toString().should.equal(
-      'florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj'
+      'flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K'
     );
 
     new URI({
-      address: '1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj',
+      address: 'oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K',
       amount: 110001000,
       message: 'Hello World',
       something: 'else'
     }).toString().should.equal(
-      'florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj?amount=1.10001&message=Hello%20World&something=else'
+      'flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K?amount=1.10001&message=Hello%20World&something=else'
     );
 
   });
 
   it('should be case insensitive to protocol', function() {
-    var uri1 = new URI('bItcOin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
-    var uri2 = new URI('florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj');
+    var uri1 = new URI('FLo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K');
+    var uri2 = new URI('flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K');
 
     uri1.address.toString().should.equal(uri2.address.toString());
   });
 
   it('writes correctly the "r" parameter on string serialization', function() {
-    var originalString = 'florincoin:mmrqEBJxUCf42vdb3oozZtyz5mKr3Vb2Em?amount=0.1&' +
+    var originalString = 'flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K?amount=0.1&' +
                          'r=https%3A%2F%2Ftest.bitpay.com%2Fi%2F6DKgf8cnJC388irbXk5hHu';
     var uri = new URI(originalString);
     uri.toString().should.equal(originalString);
   });
 
   it('displays nicely on the console (#inspect)', function() {
-    var uri = 'florincoin:1DP69gMMvSuYhbnxsi4EJEFufUAbDrEQfj';
+    var uri = 'flo:oZvZDmbZyaVVGHspkFftKx1CaoacakeR8K';
     var instance = new URI(uri);
     instance.inspect().should.equal('<URI: ' + uri + '>');
   });
